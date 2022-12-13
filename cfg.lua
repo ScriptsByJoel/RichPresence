d5fb27f9486bb90fa08d288da80151f5b07bb0c9
ScriptsByJoel <105671489+ScriptsByJoel@users.noreply.github.com>
2022-12-13 01:29:39 +0000

    Add files via upload

---
+++ cfg.lua
@@ -0,0 +1,20 @@
+cfg = {}
+--This is where you put your DiscordAppID you can get it from here https://discord.com/developers/applications
+cfg.DiscordAppID = 1052022380412809316 
+
+cfg.discordImageName = 'code'
+cfg.hoverText = 'Code Zero Roleplay'
+cfg.richPresenceText = "" ..GetPlayerServerId(PlayerId()).. " | " .. #GetActivePlayers() .. " Players"
+cfg.smallDiscordImageName = 'code'
+cfg.smallHoverText = 'Code Zero Roleplay'
+
+cfg.button1 = {
+    text = 'Zode Zero Discord',
+    url = 'https://discord.gg/bzhGEn8TCs'
+}
+
+cfg.button2 = {
+    text = 'IGNORE',
+    url = 'LINK HERE'
+}
+-- Just to clarify something, you can only have 2 buttons
